e2f54fe327f378222325c3a763314821b47a94a4
Volodymyr Bondarchuk <w.volodymyr.bondarchuk@gmail.com>
2021-01-18 22:42:51 +0200

    removed unnecessary apis

--- src/main/java/co/ratethem/controller/ReviewController.java
+++ src/main/java/co/ratethem/controller/ReviewController.java
@@ -2,7 +2,6 @@ package co.ratethem.controller;
 
 import co.ratethem.controller.rest_exception_handler.exception.EmptyValueException;
 import co.ratethem.controller.rest_exception_handler.exception.InvalidValueException;
-import co.ratethem.entity.Review;
 import co.ratethem.payload.ReviewRequest;
 import co.ratethem.repository.ReviewRepository;
 import co.ratethem.service.ReviewService;
@@ -11,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 
@@ -25,28 +23,12 @@ public class ReviewController {
 	@Autowired
 	private ReviewService reviewService;
 
-
-/*	@CrossOrigin
-	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	@ResponseStatus(value = HttpStatus.OK)
-	public void addReview(@RequestBody ReviewAddRequest reviewAddRequest) {
-		reviewService.add(reviewAddRequest);
-	}*/
-
 	@CrossOrigin
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	@ResponseStatus(value = HttpStatus.OK)
 	public void addReview(@RequestBody ReviewRequest reviewAddRequest) throws EmptyValueException, InvalidValueException {
 		reviewService.add(reviewAddRequest);
 	}
-	
-	@CrossOrigin
-	@GetMapping(value = "/all")
-	public ResponseEntity<List<Review>> viewAll() {
-	
-		return new ResponseEntity<>(reviewRepository.findAll(), HttpStatus.OK);
-	}
-	
 
 		
 	@CrossOrigin
@@ -58,23 +40,7 @@ public class ReviewController {
 		Map<String, Object> lastAdded = reviewService.getLastCreated(page, amount);
 		return new ResponseEntity<>(lastAdded, HttpStatus.OK);
 	}
-	
-	@CrossOrigin
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	@ResponseStatus(value = HttpStatus.OK)
-	public void deleteReviewById(@PathVariable Long id) {
-		
-		reviewRepository.deleteById(id);
-	}
-	
-	@CrossOrigin
-	@RequestMapping(method = RequestMethod.PUT)
-	@ResponseStatus(value = HttpStatus.OK)
-	public void editReview(@RequestBody Review featureFilm) {
 
-		reviewRepository.save(featureFilm);
-	}
-	
 }
 	
 
--- src/main/java/co/ratethem/controller/UserController.java
+++ src/main/java/co/ratethem/controller/UserController.java
@@ -1,13 +1,9 @@
 package co.ratethem.controller;
 
-import co.ratethem.entity.User;
 import co.ratethem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -17,46 +13,6 @@ public class UserController {
 	@Autowired
 	private UserRepository userDao;
 
-
-	@CrossOrigin
-	@RequestMapping(value = "/", method = RequestMethod.POST)
-	@ResponseStatus(value = HttpStatus.OK)
-	public User addUser(@RequestBody User user) {
-		return userDao.save(user);
-	}
-	
-	@CrossOrigin
-	@RequestMapping(value = "/all", method = RequestMethod.GET)
-	@ResponseStatus(value = HttpStatus.OK)
-	public List<User> viewAll() {
-	
-		return userDao.findAll();
-	}
-		
-	@CrossOrigin
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	@ResponseStatus(value = HttpStatus.OK)
-	public Optional<User> getUserById(@PathVariable Long id) {
-	
-		
-		return userDao.findById(id);
-	}
-	
-	@CrossOrigin
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	@ResponseStatus(value = HttpStatus.OK)
-	public void deleteUserById(@PathVariable Long id) {
-		
-		userDao.deleteById(id);
-	}
-	
-	@CrossOrigin
-	@RequestMapping(method = RequestMethod.PUT)
-	@ResponseStatus(value = HttpStatus.OK)
-	public void editUser(@RequestBody User user) {
-		
-		userDao.save(user);
-	}
 	
 }
 	
