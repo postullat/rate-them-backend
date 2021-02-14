7d6af81cb3115ce45208c792c6ea8673d8d28bfe
Volodymyr Bondarchuk <w.volodymyr.bondarchuk@gmail.com>
2021-02-14 20:54:30 +0200

    added sql insert

--- src/main/resources/db/2. cities.sql
+++ src/main/resources/db/2. cities.sql
@@ -1,6 +1,36 @@
-INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(1,"Lviv","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(1,"Remote","ACTIVE");
+
 INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(2,"Kyiv","ACTIVE");
 INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(3,"Kharkiv","ACTIVE");
 INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(4,"Odessa","ACTIVE");
 INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(5,"Dnipro","ACTIVE");
-INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(6,"Rivne","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(6,"Lviv","ACTIVE");
+
+
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(7,"Vinnytsia","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(8,"Ivano-Frankivsk","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(9,"Chernihiv","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(10,"Zhytomyr","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(11,"Zaporizhzhia","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(12,"Cherkasy","ACTIVE");
+
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(13,"Chernivtsi","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(14,"Uzhhorod","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(15,"Mykolaiv","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(16,"Ternopil","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(17,"Khmelnytskyi","ACTIVE");
+
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(18,"Poltava","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(19,"Sumy","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(20,"Kherson","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(21,"Lutsk","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(22,"Rivne","ACTIVE");
+
+
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(23,"Kropyvnytskyi","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(24,"Mariupol","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(25,"Kremenchuk","ACTIVE");
+INSERT INTO `ratethem`.`cities`(`id`,`name`,`status`)VALUES(26,"Kryvyi Rih","ACTIVE");
+
+
+
--- src/main/resources/db/3. companies.sql
+++ src/main/resources/db/3. companies.sql
@@ -1,3 +1,33 @@
 INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(1,"https://s.dou.ua/CACHE/images/img/static/companies/epam_logo/b84ca0a480152a50ce7495c9c1442178.png","EPAM","https://epam.com");
 INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(2,"https://s.dou.ua/CACHE/images/img/static/companies/logo_vector-01/95e1cacced85b295c83a4e05cb6b4b5b.png","SoftServe","https://www.softserveinc.com");
 INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(3,"https://s.dou.ua/CACHE/images/img/static/companies/globallogic_logo/dc0e8b9f09ed419b27c2aa8e84dd74c5.png","GlobalLogic","https://www.globallogic.com");
+
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(4,"https://s.dou.ua/CACHE/images/img/static/companies/ciklum-logo-new/e95535bbe317047306a96d5dd9541349.png","Ciklum","http://www.ciklum.com");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(5,"https://s.dou.ua/CACHE/images/img/static/companies/4040x2060_Preferred-color-logo--Digital/70febd0ccb6df1fc240c62f6f0cc06e2.png","N-iX","https://www.n-ix.com/");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(6,"https://s.dou.ua/CACHE/images/img/static/companies/Horizontal_ON_WHITE/e6a7aea9ba5f38531269fb82bdcb4d12.png","Playtika","http://playtika.com");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(7,"https://s.dou.ua/CACHE/images/img/static/companies/sigma_logo/24e4e5b034f849f0cdc853e1b836d1fc.png","Sigma Software","https://sigma.software");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(8,"https://s.dou.ua/CACHE/images/img/static/companies/Astound_Logo_360px/398c28afa624f77f84a4bccb513eafb1.png","Astound Commerce","https://careers.astoundcommerce.com/");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(9,"https://s.dou.ua/CACHE/images/img/static/companies/Untitled-2_0ff9dEg/b5cb063a49a6040b4b601fc5bb373f84.png","Svitla Systems, Inc.","https://svitla.com/career");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(10,"https://s.dou.ua/CACHE/images/img/static/companies/DOU_logo/d81a616ed79dea6ecd506163caf1e1f4.png","ELEKS","https://careers.eleks.com/");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(11,"https://s.dou.ua/CACHE/images/img/static/companies/logo_uv3ZKwW/a9f34a09d23bbc4cad8d1a87fa11d6e9.png","NIX","https://www.nixsolutions.com/");
+
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(12,"https://s.dou.ua/CACHE/images/img/static/companies/LUXOFT_DXC_logo_rgb_blue_2019/6b301bede5c0a96cdc9c9bffe6be9cc5.png","Luxoft","http://www.luxoft.com/");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(13,"https://s.dou.ua/CACHE/images/img/static/companies/amc_gray_blue_5k2/31decefa2edad4aa3a3ccae02eb09ea6.png","AMC Bridge","http://amcbridge.com.ua/");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(14,"https://s.dou.ua/CACHE/images/img/static/companies/pai-logo-full-transparent_1/66fc652afcef20831a5407f8ef3a26ae.png","People.ai","https://people.ai/");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(15,"https://s.dou.ua/CACHE/images/img/static/companies/logo-03_Jr7zxPU/be8b75d7f92ddf3ee17cde835c97e74d.png","ПриватБанк","http://www.privatbank.ua/");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(16,"https://s.dou.ua/CACHE/images/img/static/companies/logo_for_dana/e1e7516c4219adad49d1b080adb7bd4a.png","FIVERR","https://www.fiverr.com/");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(17,"https://s.dou.ua/CACHE/images/img/static/companies/200x200_rozetka-logo-c7-rgb/863881f8e1c756c97e6ff766dd08e271.png","ROZETKA","http://rozetka.com.ua/");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(18,"https://s.dou.ua/CACHE/images/img/static/companies/infopulse_logo/9b8f0da237f8ff72eb7bd55f75d9de0c.png","Infopulse","http://www.infopulse.com/");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(19,"https://s.dou.ua/CACHE/images/img/static/companies/Lohika_logo/f8e02b2940ffc3a19aacebb1040b2868.png","Lohika","http://www.lohika.com.ua/");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(20,"https://s.dou.ua/CACHE/images/img/static/companies/levi9w/61b79e07436b445b40a7f0cad41cae23.png","Levi9 Ukraine","https://www.levi9.com/");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(21,"https://s.dou.ua/CACHE/images/img/static/companies/innovecs_logo/73467f7d9311b5ed3cd77965d4b4706e.png","Innovecs","https://jobs.innovecs.com/");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(22,"https://s.dou.ua/CACHE/images/img/static/companies/dou_logo_bg2x/96c74f9421bcb4b37a91464e0197db2d.png","Trinetix","http://trinetix.com/");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(23,"https://s.dou.ua/CACHE/images/img/static/companies/%D0%BB%D0%BE%D0%B3%D0%BE_%D0%BF%D0%BD%D0%B3/9ea1dbf311bc583380caf321814d2a8b.png","Raiffeisen Bank Aval","http://www.aval.ua/");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(24,"https://s.dou.ua/CACHE/images/img/static/companies/wargaming_logo/d1eef1b6077675a9cfc3f583daafae5e.png","Wargaming.net","http://www.wargaming.com/");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(25,"https://s.dou.ua/CACHE/images/img/static/companies/%D0%BD%D0%BE%D0%B2%D0%B0_%D0%BF%D0%BE%D1%88%D1%82%D0%B0_%D1%86%D0%B5%D0%BD%D1%82%D1%80/c41ba48a39af539ce056310f45a010aa.png","Нова Пошта-Центр","https://novaposhta.ua/");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(26,"https://s.dou.ua/CACHE/images/img/static/companies/AlfaBank_LogoVERT_Red_RU-UA_ETALON_500px%D1%86%D1%86/28537e41b8b82279b9b44f714781122a.png","Альфа-Банк Украина","https://alfabank.ua/");
+
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(27,"","GlobalLogic","");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(28,"","GlobalLogic","");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(29,"","GlobalLogic","");
+INSERT INTO `ratethem`.`companies`(`id`,`logo_url`,`name`,`site_url`)VALUES(30,"","GlobalLogic","");
