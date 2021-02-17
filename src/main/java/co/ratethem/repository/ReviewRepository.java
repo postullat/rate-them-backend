54d51c42d0fca4bd7a1007665aae2cd795bafcac
Volodymyr Bondarchuk <w.volodymyr.bondarchuk@gmail.com>
2021-02-17 16:42:56 +0200

    ASC changes to DESC

--- src/main/java/co/ratethem/repository/ReviewRepository.java
+++ src/main/java/co/ratethem/repository/ReviewRepository.java
@@ -1,5 +1,6 @@
 package co.ratethem.repository;
 
+import co.ratethem.entity.Company;
 import co.ratethem.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,10 +9,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    @Query("select rw from Review rw ORDER BY rw.created ASC")
+    @Query("select rw from Review rw ORDER BY rw.created DESC")
     Page<Review> findLastAdded(Pageable pageable);
+
+    Page<Review> findReviewsByCompanyIn(List<Company> companies, Pageable pageable);
 
 }
